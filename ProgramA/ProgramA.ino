3c7d0afae756d1c146e31d4831b6de5c0e12666f
esk999 <esk08142003@gmail.com>
2024-07-23 15:03:33 +0900

    Combine Programs

--- ProgramA/ProgramA.ino
+++ ProgramA/ProgramA.ino
@@ -1,5 +1,6 @@
 #include <Wire.h>
 #include <ZumoShieldN.h>
+#include "Route.h"
 
 int route[10] = {0};
 char commands[13] = {0};
@@ -83,7 +84,6 @@ void GetCommand(int route[11]){
         break;
     }
   }
-  
 }
 
 void setup() {
@@ -95,10 +95,6 @@ void setup() {
   button.waitForPress();
   delay(100);
   setup_state_machine();
-  while(next_command(commands)){
-    Serial.println(commands);
-  }
-
 }
 
 void loop(){
