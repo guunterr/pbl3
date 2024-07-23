4511df5fba977c4132c71f76865b792a181c23c5
guunterr <gerardrglowacki@gmail.com>
2024-07-23 13:41:41 +0900

    Add route generation code

--- ProgramA/Route.ino
+++ ProgramA/Route.ino
@@ -1,74 +1,119 @@
 #include <Wire.h>
 #include <ZumoShieldN.h>
+#include "route.h"
 
-typedef struct{
-  int array[12];
-  int index;
-} Queue;
-
-int push(Queue *q, int item);
-bool isEmpty(Queue* q);
-int pop(Queue* q);
+void get_commands(int start, int end, direction_t starting_direction, char commands[13]);
+char command_from_directions(direction_t current_direction, direction_t new_direction);
 
 
-int push(Queue *q, int item){
-  if (q->index > 11){
-    Serial.println("Reading from empty queue!");
-    return 0;
-  } else{
-    q->array[q->index] = item;
-    q->index++;
-    return 1;
+void runme() {
+  char commands[13] = { '0' };
+  Serial.println("Testing");
+  get_commands(1, 0xa, UP, commands);
+  Serial.println(commands);
+  button.waitForPress();
+  delay(100);
+}
+
+void xy_from_vertex(int vertex, int* x, int* y) {
+  *x = vertex % 4;
+  *y = vertex / 4;
+}
+
+void get_commands(int start, int end, direction_t starting_direction, char commands[13]) {
+  int x= 0; int y= 0; int end_x= 0; int end_y= 0;
+  xy_from_vertex(start, &x, &y);
+  int i = 0;
+  xy_from_vertex(end, &end_x, &end_y);
+  direction_t direction = starting_direction;
+  Serial.println(x);
+  Serial.println(y);
+  Serial.println(end_x);
+  Serial.println(end_y);
+  while (x < end_x) {
+    x++;
+    commands[i++] = command_from_directions(direction, RIGHT);
   }
-}
-
-bool isEmpty(Queue* q){
-  return (q->index == 0);
-}
-
-int pop(Queue* q){
-  if (q->index == 0){
-    return -1;
-  } else{
-    q->index--;
-    int item = q->array[q->index];
-    return item;
+  while (x > end_x) {
+    x--;
+    commands[i++] = command_from_directions(direction, LEFT);
   }
+  while (y < end_y) {
+    y++;
+    commands[i++] = command_from_directions(direction, UP);
+  }
+  while (y > end_y) {
+    y--;
+    commands[i++] = command_from_directions(direction, DOWN);
+  }
+  commands[i] = '.';
 }
 
-int graph[12][12] = {
-// 0,1,2,3,4,5,6,7,8,9,a,b
-  {1,1,0,0,1,0,0,0,0,0,0,0}, //0
-  {1,1,1,0,0,0,0,0,0,0,0,0}, //1
-  {0,1,1,1,0,0,0,0,0,0,0,0}, //2
-  {0,0,1,1,0,0,0,1,0,0,0,0}, //3
-  {1,0,0,0,1,1,0,0,1,0,0,0}, //4
-  {0,0,0,0,1,1,1,0,0,0,0,0}, //5
-  {0,0,0,0,0,1,1,1,0,0,1,0}, //6
-  {0,0,0,1,0,0,1,1,0,0,0,0}, //7
-  {0,0,0,0,1,0,0,0,1,1,0,0}, //8
-  {0,0,0,0,0,0,0,0,1,1,0,0}, //9
-  {0,0,0,0,0,0,1,0,0,0,1,1}, //a
-  {0,0,0,0,0,0,0,0,0,0,1,1}, //b
-// 0,1,2,3,4,5,6,7,8,9,a,b
-};
-
-void bfs(int start, int end){
-  int predecessor[12];
-  bool visited[12] = {false,false,false,false,false,false,false,false,false,false,false,false};
-  Queue unvisited = {{0}, 0};
-  push(&unvisited,start);
-  while (!isEmpty(&unvisited)){
-    int current = pop(&unvisited);
-    if(!visited[current]){
-      visited[current] = true;
-      // if (current == end) return;
-      for (int i = 0; i < 12;i++){
-        if (graph[current][i] == 1 && !visited[i]){
-          push(&unvisited,i);
-          predecessor[i] = current;
-        } 
+char command_from_directions(direction_t current_direction, direction_t new_direction) {
+  switch (current_direction) {
+    case UP:
+      switch (new_direction) {
+        case UP:
+          return 'f';
+          break;
+        case RIGHT:
+          return 'r';
+          break;
+        case DOWN:
+          return 'b';
+          break;
+        case LEFT:
+          return 'l';
+          break;
       }
-    }
+      break;
+    case RIGHT:
+      switch (new_direction) {
+        case UP:
+          return 'l';
+          break;
+        case RIGHT:
+          return 'f';
+          break;
+        case DOWN:
+          return 'r';
+          break;
+        case LEFT:
+          return 'b';
+          break;
+      }
+      break;
+    case DOWN:
+      switch (new_direction) {
+        case UP:
+          return 'b';
+          break;
+        case RIGHT:
+          return 'l';
+          break;
+        case DOWN:
+          return 'f';
+          break;
+        case LEFT:
+          return 'r';
+          break;
+      }
+      break;
+    case LEFT:
+      switch (new_direction) {
+        case UP:
+          return 'r';
+          break;
+        case RIGHT:
+          return 'b';
+          break;
+        case DOWN:
+          return 'l';
+          break;
+        case LEFT:
+          return 'f';
+          break;
+      }
+      break;
   }
 }
